db149ebe9762d1fd34e1cfbbb7e5c6ca9eb0db1c
Gustavo Nascimento <gustavo.nascprojetos7@gmail.com>
2025-10-05 07:58:26 -0300

    Add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import PostList from './Pages/PostList';
+import CreatePost from './Pages/CreatePost';
+import EditPost from './Pages/EditPostList';
+
+import Header from './Components/Header/header'; // Uso geral!!! | vai em tds as pages.
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header /> 
+
+      {/* Pages para renderizar... */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/posts" element={<PostList />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/edit-post/:id" element={<EditPost />} />
+      </Routes>
+
+      {/* Talvez um footer no futuro... */}
     </>
   )
 }
